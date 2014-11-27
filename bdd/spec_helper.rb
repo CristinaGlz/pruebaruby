2bb5d5562ba9307ef1a9bbb25ca76b20be80baab
CristinaGlz <cgm00001@gmail.com>
2014-11-27 02:36:25 -0600

    Update spec_helper.rb

--- bdd/spec_helper.rb
+++ bdd/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'simplecov'
-require 'webmock'
-require 'vcr'
+
 
 class InceptionFormatter
   def format(result)
@@ -23,59 +22,4 @@ def setup_formatter
   end
 end
 
-setup_formatter
 
-require 'coveralls'
-
-VCR.configure do |c|
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
-  c.hook_into :webmock
-end
-
-RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-  config.include WebMock::API
-  config.expect_with :rspec do |c|
-    c.syntax = [:should, :expect]
-  end
-  config.mock_with :rspec do |c|
-    c.syntax = [:should, :expect]
-  end
-  config.after(:suite) do
-    WebMock.disable!
-  end
-end
-
-def stub_api_post
-  body = "{\"message\":\"\",\"url\":\"\"}"
-  stub_request(:post, Coveralls::API::API_BASE+"/jobs").with(
-    :body => /.+/,
-    :headers => {
-      'Accept'=>'*/*; q=0.5, application/xml',
-      'Accept-Encoding'=>'gzip, deflate',
-      'Content-Length'=>/.+/,
-      'Content-Type'=>/.+/,
-      'User-Agent'=>'Ruby'
-    }
-  ).to_return(:status => 200, :body => body, :headers => {})
-end
-
-def silence
-  return yield if ENV['silence'] == 'false'
-
-  silence_stream(STDOUT) do
-    yield
-  end
-end
-
-module Kernel
-  def silence_stream(stream)
-    old_stream = stream.dup
-    stream.reopen(RUBY_PLATFORM =~ /mswin/ ? 'NUL:' : '/dev/null')
-    stream.sync = true
-    yield
-  ensure
-    stream.reopen(old_stream)
-  end
-end
