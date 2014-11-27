34f735976ddab1984a5b66a1216701f320818781
CristinaGlz <cgm00001@gmail.com>
2014-11-27 06:22:35 -0600

    Update spec_helper.rb

--- bdd/feature/test/spec_helper.rb
+++ bdd/feature/test/spec_helper.rb
@@ -1,5 +1,8 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'simplecov'
+require 'codeclimate-test-reporter'
 require 'coveralls'
+SimpleCov.add_filter 'vendor'
+SimpleCov.formatters = []
+SimpleCov.start CodeClimate::TestReporter.configuration.profile
 Coveralls.wear!
 
