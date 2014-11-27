208d5a1107bff867b922abbcd6cacacf6364c986
CristinaGlz <cgm00001@gmail.com>
2014-11-27 05:38:02 -0600

    Update spec_helper.rb

--- bdd/spec_helper.rb
+++ bdd/spec_helper.rb
@@ -1,25 +1,3 @@
-require 'simplecov'
-
-
-class InceptionFormatter
-  def format(result)
-    Coveralls::SimpleCov::Formatter.new.format(result)
-  end
-end
-
-def setup_formatter
-  SimpleCov.formatter = if ENV['TRAVIS'] || ENV['COVERALLS_REPO_TOKEN']
-    InceptionFormatter
-  else
-    SimpleCov::Formatter::HTMLFormatter
-  end
-
-  # SimpleCov.start 'test_frameworks'
-  SimpleCov.start do
-    add_filter do |source_file|
-      source_file.filename =~ /spec/ && !(source_file.filename =~ /fixture/)
-    end
-  end
-end
-
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
