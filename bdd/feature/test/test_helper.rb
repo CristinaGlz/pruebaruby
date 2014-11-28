13f2ca000d99b8b3c88f0d5b6db6c977b6552793
jorgitopeke <jorgito.c.2011@gmail.com>
2014-11-28 04:38:12 -0600

    Update test_helper.rb

--- bdd/feature/test/test_helper.rb
+++ bdd/feature/test/test_helper.rb
@@ -1,2 +1,8 @@
 require "codeclimate-test-reporter"
+require 'simplecov'
+require 'coveralls'
+CodeClimate::TestReporter.configuration.profile
 CodeClimate::TestReporter.start
+SimpleCov.start
+Coveralls.start
+Coveralls.wear!
