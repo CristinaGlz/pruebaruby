9a2a1d873f31c57b2d5a64530fa81777a5c1028a
jorgitopeke <jorgito.c.2011@gmail.com>
2014-11-28 00:45:32 -0600

    Create IngresosExentos.rb

---
+++ bdd/feature/step_definitions/IngresosExentos.rb
@@ -0,0 +1,12 @@
+class IngresosExentos
+  $exe
+
+  def calcularIngresosExentos(diasAguinaldo,diasPrimaV)
+    	$aguiexentos = diasAguinaldo * 45.24
+	$primaexento = diasPrimaV * 45.24
+  end
+  def resultadoExentos
+       	$aguiexentos
+   	$primaexento
+  end
+end
